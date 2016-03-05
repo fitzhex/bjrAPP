7ffe71bdd62aa0d98f6d073bdfaf3764ddbed7c0
fitzhex <fitz.hex@gmail.com>
2016-03-06 00:43:11 +0800

    initial
    initial

---
+++ app/src/main/java/com/fitzhex/banjirapp/MapsActivity.java
@@ -0,0 +1,91 @@
+package com.fitzhex.banjirapp;
+
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.IBinder;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    //avoid onresume calling onMapReady when first time laucnh
+    private int flag = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        flag = 1;
+
+        GPSTracker gps = new GPSTracker(this);
+        gps.setMapVar(googleMap);
+
+        LatLng location = new LatLng(gps.getLatitude(), gps.getLongitude());
+
+        //if not gps enabled prompt user
+        if (gps.canGetLocation() == false) {
+            gps.showSettingsAlert();
+        } else {
+            setupMap(googleMap, location);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+        if (flag == 1) {
+
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    private void setupMap(GoogleMap googleMap, LatLng location) {
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        // Add a marker in Sydney and move the camera
+
+        mMap.addMarker(new MarkerOptions().position(location).title("You are here!"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 15.0f));
+
+
+    }
+
+    public void updateMarker(GoogleMap mMap, LatLng loc) {
+        setupMap(mMap, loc);
+    }
+
+
+}
